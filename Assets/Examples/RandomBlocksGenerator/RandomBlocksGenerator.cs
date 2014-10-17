6b3c92ec9478510666b391c987d33e32fb3cd2ca
Wilbur Yu <yuwilbur@gmail.com>
2014-10-16 22:35:18 -0700

    Added colors for RandomBlocksGenerator

--- Assets/Examples/RandomBlocksGenerator/RandomBlocksGenerator.cs
+++ Assets/Examples/RandomBlocksGenerator/RandomBlocksGenerator.cs
@@ -10,14 +10,14 @@ public class RandomBlocksGenerator : MonoBehaviour {
 
   private Color[] list_of_colors = 
   {
-    new Color(1.00f, 1.00f, 0.94f), // (255,255,240) Ivory
-    new Color(0.96f, 0.96f, 0.86f), // (245,245,220) Beige
-    new Color(0.96f, 0.87f, 0.70f), // (245,222,179) Wheat
-    new Color(0.82f, 0.71f, 0.55f), // (210,180,140) Tan
-    new Color(0.76f, 0.69f, 0.57f), // (195,176,145) Khaki
-    new Color(0.75f, 0.75f, 0.75f), // (192,192,192) Silver
-    new Color(0.50f, 0.50f, 0.50f), // (128,128,128) Gray
-    new Color(0.27f, 0.27f, 0.27f), // (070,070,070) Charcoal
+    //new Color(1.00f, 1.00f, 0.94f), // (255,255,240) Ivory
+    //new Color(0.96f, 0.96f, 0.86f), // (245,245,220) Beige
+    //new Color(0.96f, 0.87f, 0.70f), // (245,222,179) Wheat
+    //new Color(0.82f, 0.71f, 0.55f), // (210,180,140) Tan
+    //new Color(0.76f, 0.69f, 0.57f), // (195,176,145) Khaki
+    //new Color(0.75f, 0.75f, 0.75f), // (192,192,192) Silver
+    //new Color(0.50f, 0.50f, 0.50f), // (128,128,128) Gray
+    //new Color(0.27f, 0.27f, 0.27f), // (070,070,070) Charcoal
     new Color(0.00f, 0.00f, 1.00f), // (000,000,255) Navy Blue
     new Color(0.02f, 0.30f, 0.62f), // (008,076,158) Royal Blue
     new Color(0.00f, 0.00f, 0.80f), // (000,000,205) Medium Blue
@@ -33,7 +33,15 @@ public class RandomBlocksGenerator : MonoBehaviour {
     new Color(0.85f, 0.65f, 0.13f), // (218,165,032) Goldenrod
     new Color(1.00f, 0.50f, 0.31f), // (255,127,080) Coral
     new Color(0.98f, 0.50f, 0.45f), // (250,128,114) Salmon
-    new Color(0.99f, 0.06f, 0.75f) // (252,015,192) Hot Pink
+    new Color(0.99f, 0.06f, 0.75f), // (252,015,192) Hot Pink
+    new Color(1.00f, 0.47f, 1.00f), // (255,119,255) Fuchsia
+    new Color(0.80f, 0.53f, 0.60f), // (204,136,153) Puce
+    new Color(0.88f, 0.69f, 1.00f), // (224,176,225) Mauve
+    new Color(0.71f, 0.49f, 0.86f), // (181,126,220) Lavender
+    new Color(0.52f, 0.19f, 0.47f), // (132,049,121) Plum
+    new Color(0.29f, 0.00f, 0.51f), // (075,000,130) Indigo
+    new Color(0.50f, 0.00f, 0.00f), // (127,000,000) Maroon
+    new Color(0.86f, 0.08f, 0.24f)  // (220,020,060) Crimson
   };
 
   private void Reset()
@@ -73,7 +81,7 @@ public class RandomBlocksGenerator : MonoBehaviour {
       cube.AddComponent<Rigidbody>();
       cube.rigidbody.useGravity = false;
 
-      cube.renderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+      cube.renderer.material.color = list_of_colors[Random.Range(0,list_of_colors.Length - 1)];
     }
   }
 
